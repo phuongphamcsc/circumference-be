633c4aa6dd147c3633ba347ccdef8693388c60f3
phuongphamcsc <phamminhphuongstudent@gmail.com>
2021-09-30 23:17:11 +0800

    add heroku server swagger

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const options = {
     },
     servers: [
       {
-        url: "https://pi-spigot.herokuapp.com"
+        url: "https://circumference-sun.herokuapp.com/"
       }
     ]    
   },
